5614179b08da4060997fb455e8ec196563aecb1a
Julia Babina <u.babina55@gmail.com>
2022-12-13 10:57:30 +0500

    add N5

---
+++ Homework/N5.java
@@ -0,0 +1,83 @@
+import java.util.HashMap;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class N5 {
+    public static void main(String[] args) {
+        newMap();
+        comparingArrays();
+    }
+
+    public static void newMap() {
+        System.out.println("HashMap");
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(0, "Maria");
+        map.put(1, "Maxim");
+        map.put(2, "Irina");
+        map.putIfAbsent(3, "Alex");
+        map.forEach((k, v) -> System.out.println("Key: " + k + " Volume: " + v));
+        map.replaceAll((k, v) -> v = v + "!");
+        map.forEach((k, v) -> System.out.println("Key: " + k + " Volume: " + v));
+
+        System.out.println("TreeMap");
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(0, "Blue");
+        treeMap.put(1, "Black");
+        treeMap.put(2, "White");
+        treeMap.putIfAbsent(3, "Red");
+        treeMap.forEach((k, v) -> System.out.println("Key: " + k + " Volume: " + v));
+        treeMap.replaceAll((k, v) -> v = v + "!");
+        treeMap.forEach((k, v) -> System.out.println("Key: " + k + " Volume: " + v));
+    }
+
+    private static void comparingArrays() {
+        int count = 10001;
+        Random random = new Random();
+        HashMap<Integer, String> newMap = new HashMap<>();
+        for (int i = 0; i < count; i++) {
+            int key = random.nextInt(count);
+            newMap.put(key, "value");
+        }
+
+        TreeMap<Integer, String> newTreeMap = new TreeMap<>();
+        for (int i = 0; i < count; i++) {
+            int key = random.nextInt(count);
+            newTreeMap.put(key, "value");
+        }
+
+        float start = System.currentTimeMillis();
+        newMap.forEach((k, v) -> System.out.print(k + " " + v));
+        float stop = System.currentTimeMillis() - start;
+        System.out.println();
+        System.out.println("Время прямого перебора HashMap " + stop + " мс.");
+
+
+        start = System.currentTimeMillis();
+        newTreeMap.forEach((k, v) -> System.out.print(k + " " + v));
+        stop = System.currentTimeMillis() - start;
+        System.out.println();
+        System.out.println("Время прямого перебора TreeMap " + stop + " мс.");
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            int key = random.nextInt(count);
+            System.out.print(newMap.get(key));
+        }
+        stop = System.currentTimeMillis() - start;
+        System.out.println();
+        System.out.println("Время рандомного перебора HashMap " + stop + " мс.");
+
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < count; i++) {
+            int key = random.nextInt(count);
+            System.out.print(newTreeMap.get(key));
+        }
+        stop = System.currentTimeMillis() - start;
+        System.out.println();
+        System.out.println("Время рандомного перебора TreeMap " + stop + " мс.");
+
+    }
+
+
+}
